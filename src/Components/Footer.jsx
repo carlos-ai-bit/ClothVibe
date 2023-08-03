3fe4b79cd50fa6265a60ea29cc1c29a81ce15af2
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-08-03 14:38:46 -0400

    makes changes to the footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,12 @@
 function Footer() {
   return (
-    <div className="items-center">
+    <div className="">
       {" "}
-      <form action="" className="flex items-center">
-        <div className="items-center  ml-20">
-          <img className="w-48 h-56" src="src/assets/7938113 (1).jpg" alt="" />
-          <h4 className="text-black-200  ml-20">
+      <form action="" className="flex flex-row ml-64 items-center">
+        
+        <div className="items-center  ml-10">
+          <img className="w-36 h-40" src="src/assets/7938113 (1).jpg" alt="" />
+          <h4 className="text-black-200  ml-10 text-center">
             LEADING BEST <br />
             CLOTHING <br />
             BRANDS ACROSS <br /> THE GLOBE
@@ -27,14 +28,14 @@ function Footer() {
             />
           </div>
         </div>
-        <div className="p-20">
+        <div className="p-20 ml-10">
           <h3 className="mb-6 text-sm text-black-200">CONTACT US</h3>
           <div className="mt-20">
             <h3>0738203784</h3>
           </div>
         </div>
       </form>
-      <div className="text-center text-black-200">
+      <div className="text-center text-black-200 ml-10">
         <h4>GET THE BEST CLOTHING BRAND WITH US</h4>
       </div>
       <div className="text-center m-3">
@@ -53,13 +54,15 @@ function Footer() {
       <div>
         <hr className="mt-4 font-bold" />
       </div>
-      <div className="flex text-gray-600 p-3 text-center">
+      <div className="ml-64  items-center">
+      <div className="flex text-gray-600 p-3  ml-64">
         <h4 className="p-3">Website terms</h4>
 
-        <h4 className="p-3">Privacy Policy</h4>
+        <h4 className="p-3 ">Privacy Policy</h4>
       </div>
-      <span className="text-sm  text-gray-500 sm:text-center p-4 dark:text-gray-400">@ 2023 <a href="https://flowbite.com/" className="hover:underline">Samori</a>. All Rights Reserved.
+      <span className="text-sm  ml-64  text-gray-500 sm:text-center p-4 dark:text-gray-400">@ 2023 <a href="https://flowbite.com/" className="hover:underline">Samori</a>. All Rights Reserved.
     </span>
+    </div>
     </div>
   );
 }
