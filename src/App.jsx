732e8171957bc9fc454270fad6befe5099bf3cf9
Hoperaisers <abdallahsafari@gmail.com>
2023-08-04 10:37:36 +0300

    chsnges

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import "./App.css";
-// import Navbar from "./Components/Navbar";
-// import Shoes from "./Components/Shoes";
-// import Sweaters from "./Components/Sweaters";
-// import Tops from "./Components/Tops";
-// import Bags from "./Components/Bags";
+import Navbar from "./Components/Navbar";
+import Shoes from "./Components/Shoes";
+import Sweaters from "./Components/Sweaters";
+import Tops from "./Components/Tops";
+import Bags from "./Components/Bags";
 function App() {
   return (
     <>
-      {/* <Navbar />
+      <Navbar />
       <Sweaters/>
       <Shoes/>
       <Tops/>
-      <Bags/> */}
+      <Bags/>
     </>
   );
 }
