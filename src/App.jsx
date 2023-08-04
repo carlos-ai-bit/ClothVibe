4b7816fe6449fab3d6ca7436838514fe34ad218b
zuleika <njokizuleika@gmail.com>
2023-08-04 13:56:21 +0300

    adds a products component page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import "./App.css";
 
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 
-import Shoes from "./Components/Shoes";
-import Sweaters from "./Components/Sweaters";
-import Tops from "./Components/Tops";
-import Bags from "./Components/Bags";
-
-import All from "./Components/All";
-import UnderWear from "./Components/UnderWear";
+// import Shoes from "./Components/Shoes";
+// import Sweaters from "./Components/Sweaters";
+// import Tops from "./Components/Tops";
+// import Bags from "./Components/Bags";
+// import Navbar from "./Components/Navbar"
+// import All from "./Components/All";
+// import UnderWear from "./Components/UnderWear";
 function App() {
   return (
     
       <>
+      {/* <Navbar/>
         <Routes>
           <Route exact path="/all" element={<All/> }/>
           <Route exact path="/sweaters" element={<Sweaters/>} />
@@ -20,7 +21,7 @@ function App() {
           <Route exact path="/tops" element={<Tops/>} />
           <Route exact path="/bags" element={<Bags/>} />
           <Route exact path="/UnderWear" element={<UnderWear/>} />
-          </Routes>
+          </Routes> */}
       </>
 
   );
--- src/Components/All.jsx
+++ src/Components/All.jsx
@@ -1,17 +1,62 @@
 
+import ProductsView from "./ProductsView";
+// eslint-disable-next-line no-unused-vars
+import {sweaterData} from "./SweaterData";
+import {shoesData }from "./SweaterData";
+import {topsData }from "./SweaterData";
+import {bagsData }from "./SweaterData";
+import {underwearData }from "./SweaterData";
+
 function All() {
   return (
-   <>
-   <div className="w-full h-full"
-      >
- <img
- src="src/Assets/clothes asthetic.jpg"
- className="w-full h-full"
- />
+                  
+      <div className="w-full h-full flex flex-wrap">
+      
+     
+    
+     
+      {shoesData.map((shoe) => (
+        <div className=" grid grid-col-3 shadow mx-8 mb-4 py-4 w-56 rounded-lg " key={shoe.id}>
+          <img src={shoe.imageSrc} 
+          alt={shoe.name} />
+          {/* <p>{shoe.price}</p> */}
+        </div>
+        
+      ))}
 
-   </div>
-   </>
-  )
+    
+      
+      {topsData.map((top) => (
+        <div className="shadow mx-8 mb-4 py-4 w-56 grid grid-col-3 rounded-lg" key={top.id}>
+          <img src={top.imageSrc}
+           alt={top.name} />
+          {/* <p>{top.price}</p> */}
+        </div>
+      ))}
+
+     
+      
+      {bagsData.map((bag) => (
+        <div className="shadow mx-8 mb-4 py-4 w-56  grid grid-col-3 rounded-lg" key={bag.id}>
+          <img src={bag.imageSrc} 
+          alt={bag.name} />
+          {/* <p>{bag.price}</p> */}
+        </div>
+      ))}
+
+     
+   
+      {underwearData.map((underwear) => (
+        <div className="shadow mx-8 mb-4 py-4 w-56  grid grid-col-3 rounded-lg" key={underwear.id}>
+          <img src={underwear.imageSrc} 
+          alt={underwear.name} />
+          {/* <p>{underwear.price}</p> */}
+        </div>
+      ))}
+
+      <ProductsView />
+    </div>
+  );
 }
 
-export default All
+export default All;
