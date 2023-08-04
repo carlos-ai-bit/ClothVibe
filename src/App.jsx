aeecbec4e3ceab0fd70abbd20b1e3474e1018ad2
Hoperaisers <abdallahsafari@gmail.com>
2023-08-04 10:54:09 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import HomePage from "./Components/HomePage";
+// import HomePage from "./Components/HomePage";
 function App() {
   return (
     <>
-      <HomePage />
+      {/* <HomePage /> */}
     </>
   );
 }
