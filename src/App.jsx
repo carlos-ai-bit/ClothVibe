2b5577423c259822534643693c312771ae8166ad
Hoperaisers <abdallahsafari@gmail.com>
2023-08-07 16:42:39 +0300

    updates payment.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import Loginpage from "./Components/Loginpage";
 //import Signupage from "./Components/Signupage";
-import AddCart from "./Components/AddCart";
+// import AddCart from "./Components/AddCart";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Shoes from "./Components/Shoes";
@@ -11,14 +11,16 @@ import HomePage from "./Components/HomePage";
 import All from "./Components/All";
 import UnderWear from "./Components/UnderWear";
 import Cartview from "./Components/Cartview";
+import Payment from "./Components/Payment";
 function App() {
   return (
     <>
-       <AddCart/> 
-       {/*<Loginpage/>*/} 
-       {/*<Signupage/>*/} 
+      {/* <AddCart/>  */}
+      {/*<Loginpage/>*/}
+      {/*<Signupage/>*/}
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/payment" element={<Payment />} />
         <Route exact path="/all" element={<All />} />
         <Route exact path="/sweaters" element={<Sweaters />} />
         <Route exact path="/shoes" element={<Shoes />} />
