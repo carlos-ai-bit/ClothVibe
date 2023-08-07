d672c7c53dc065fd6433b89e0d4f5c4d679b562d
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-08-07 15:11:46 -0400

    makes changes to the footer page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import './App.css'
-// import Categories from './Components/Categories'
-// import Footer from './Components/Foot
-//import Loginpage from "./Components/Loginpage";
-//import Signupage from "./Components/Signupage";
-import AddCart from "./Components/AddCart";
 import "./App.css";
+
+import Footer from "./Components/Footer";
+import Loginpage from "./Components/Loginpage";
+import Signupage from "./Components/Signupage";
+import AddCart from "./Components/AddCart";
 import { Route, Routes } from "react-router-dom";
 import Shoes from "./Components/Shoes";
 import Sweaters from "./Components/Sweaters";
@@ -16,13 +15,13 @@ import UnderWear from "./Components/UnderWear";
 function App() {
   return (
     <>
-       <AddCart/> 
-       {/*<Loginpage/>*/} 
-       {/*<Signupage/>*/} 
-       {/* <Categories/> */}
-       {/* <Footer/> */}
+      <AddCart/> 
+      { <Loginpage/>} 
+       {<Signupage/>} 
+   
+      {<Footer />}
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
+         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/all" element={<All />} />
         <Route exact path="/sweaters" element={<Sweaters />} />
         <Route exact path="/shoes" element={<Shoes />} />
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,30 +2,11 @@ function Footer() {
   return (
     <div className="">
       {" "}
-      <form action="" className="flex flex-row ml-64 items-center">
-        
-        <div className="items-center  ml-10">
+      <form action="" className="flex flex-row ml-64  px-30 items-center">
+        <div className="items-center  ml-20">
           <img className="w-36 h-40" src="src/assets/7938113 (1).jpg" alt="" />
           <h4 className="text-black-200  ml-10 text-center">
             LEADING BEST <br />
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
             CLOTHING <br />
             BRANDS ACROSS <br /> THE GLOBE
           </h4>
@@ -46,14 +27,14 @@ function Footer() {
             />
           </div>
         </div>
-        <div className="p-20 ml-10">
+        <div className="p-20 ml-20">
           <h3 className="mb-6 text-sm text-black-200">CONTACT US</h3>
           <div className="mt-20">
             <h3>0738203784</h3>
           </div>
         </div>
       </form>
-      <div className="text-center text-black-200 ml-10">
+      <div className="text-center text-black-200 px-38 ">
         <h4>GET THE BEST CLOTHING BRAND WITH US</h4>
       </div>
       <div className="text-center m-3">
@@ -64,7 +45,7 @@ function Footer() {
         />
         <button
           type="submit"
-          className="inline-flex items-center py-2 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-full border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="inline-flex items-center py-2 px-3 ml-2 text-sm font-medium text-white bg-blue-500 rounded-full border border-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-500 dark:focus:ring-blue-500"
         >
           Subscribe
         </button>
@@ -73,14 +54,19 @@ function Footer() {
         <hr className="mt-4 font-bold" />
       </div>
       <div className="ml-64  items-center">
-      <div className="flex text-gray-600 p-3  ml-64">
-        <h4 className="p-3">Website terms</h4>
+        <div className="flex text-gray-600 p-3  ml-64">
+          <h4 className="p-3">Website terms</h4>
 
-        <h4 className="p-3 ">Privacy Policy</h4>
+          <h4 className="p-3 ">Privacy Policy</h4>
+        </div>
+        <span className="text-sm  ml-64  text-gray-500 sm:text-center p-4 dark:text-gray-400">
+          @ 2023{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            Samori
+          </a>
+          . All Rights Reserved.
+        </span>
       </div>
-      <span className="text-sm  ml-64  text-gray-500 sm:text-center p-4 dark:text-gray-400">@ 2023 <a href="https://flowbite.com/" className="hover:underline">Samori</a>. All Rights Reserved.
-    </span>
-    </div>
     </div>
   );
 }
