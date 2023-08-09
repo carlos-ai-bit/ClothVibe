715f68fadc5c481663a2a2bf4ee11074c6fde72e
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-08-09 10:26:25 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import './App.css'
-// import Categories from './Components/Categories'
-import Footer from './Components/Footer'
-
+import "./App.css";
+import Footer from "./Components/Footer";
+import Loginpage from "./Components/Loginpage";
+import Signupage from "./Components/Signupage";
+// import AddCart from "./Components/AddCart";
+import { Route, Routes } from "react-router-dom";
+import Shoes from "./Components/Shoes";
+import Sweaters from "./Components/Sweaters";
+import Tops from "./Components/Tops";
+import Bags from "./Components/Bags";
+import HomePage from "./Components/HomePage";
+import All from "./Components/All";
+import UnderWear from "./Components/UnderWear";
+import Cartview from "./Components/Cartview";
+import Payment from "./Components/Payment";
 function App() {
-
-
   return (
     <>
-     {/* <Categories/> */}
-     <Footer/>
+      {/* <AddCart/>  */}
+      {<Loginpage />}
+      {<Signupage />}
+      {<Footer />}
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/payment" element={<Payment />} />
+        <Route exact path="/all" element={<All />} />
+        <Route exact path="/sweaters" element={<Sweaters />} />
+        <Route exact path="/shoes" element={<Shoes />} />
+        <Route exact path="/tops" element={<Tops />} />
+        <Route exact path="/bags" element={<Bags />} />
+        <Route exact path="/UnderWear" element={<UnderWear />} />
+        <Route exact path="/Cartview" element={<Cartview />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
