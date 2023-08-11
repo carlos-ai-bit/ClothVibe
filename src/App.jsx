eb15447df7fb184e014bf469bf3a7511394679b8
lordessantoinette@gmail.com <lordessantoinette>
2023-08-11 11:43:20 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-//import Loginpage from "./Components/Loginpage";
-//import Signupage from "./Components/Signupage";
+import Loginpage from "./Components/Loginpage";
+import Signupage from "./Components/Signupage";
 import AddCart from "./Components/AddCart";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
@@ -10,13 +10,17 @@ import Bags from "./Components/Bags";
 import HomePage from "./Components/HomePage";
 import All from "./Components/All";
 import UnderWear from "./Components/UnderWear";
+//import SlideOver from "./Components/SlideOver";
+
 function App() {
   return (
     <>
-       <AddCart/> 
-       {/*<Loginpage/>*/} 
-       {/*<Signupage/>*/} 
+       
+       <Loginpage/> 
+       <Signupage/> 
+        {/*<AddCart/>*/}
       <Routes>
+
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/all" element={<All />} />
         <Route exact path="/sweaters" element={<Sweaters />} />
@@ -24,6 +28,9 @@ function App() {
         <Route exact path="/tops" element={<Tops />} />
         <Route exact path="/bags" element={<Bags />} />
         <Route exact path="/UnderWear" element={<UnderWear />} />
+        <Route exact path="/AddCart" element={<AddCart />} />
+        <Route exact path="/Signupage" element={<Signupage />} />
+        <Route exact path="/Loginpage" element={<Loginpage />} />
       </Routes>
     </>
   );
