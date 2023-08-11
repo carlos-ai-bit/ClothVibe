8704610e177caeb75e3c3be681b86f100f2e0244
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-08-11 12:24:13 +0300

    updates

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -31,6 +31,8 @@ function Footer() {
           <h3 className="mb-6 text-sm text-black-200">CONTACT US</h3>
           <div className="mt-20">
             <h3>0738203784</h3>
+            <h4>0704160306</h4>
+            <h5>0112954775</h5>
           </div>
         </div>
         <div className="p-20 ml-10">
@@ -55,13 +57,13 @@ function Footer() {
       <div>
         <hr className="font-bold" />
       </div>
-      <div className="text-center bg-blue-50 ml-50 ">
+      <div className="text-center bg-blue-50 ml-50">
         <div className="flex text-gray-600 p-3 text-center ml-64 ">
           <h4 className="p-2 ml-64 "> Privacy Policy Website terms</h4>
 
           
         </div>
-        <span className="text-sm  ml-50  text-gray-500 sm:text-center p-4 dark:text-gray-400">
+        <span className="text-sm  ml-64 text-gray-500 sm:text-center p-4 dark:text-gray-400">
           @ 2023{" "}
           <a href="https://flowbite.com/" className="hover:underline">
             Samori
