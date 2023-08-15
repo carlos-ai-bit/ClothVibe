217f4618a1471e1dd34f871e013561129fe96d96
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-08-15 14:31:50 +0300

    updates login and signup

--- src/Components/Signupage.jsx
+++ src/Components/Signupage.jsx
@@ -10,7 +10,7 @@ function Signupage() {
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-20 w-20"
+            className="mx-auto h-30 w-30"
             src="https://i.pinimg.com/236x/83/0e/f6/830ef61c793532145298d984f1c66131.jpg"
             alt="Logo"
           />
@@ -81,6 +81,7 @@ function Signupage() {
       </div>
       <div className=" text-center">
         <button
+      
           type="submit"
           className="mt-8 center rounded-md bg-blue-500 px-5 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
